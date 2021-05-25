5177ff0883ed2239a4328d3509c72b766551d73b
merveucer <merveucer.txt@gmail.com>
2021-05-25 20:58:34 +0300

    🎉 Initial commit

---
+++ src/test/java/io/kodlama/hrms/HrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kodlama.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
