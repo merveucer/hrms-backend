af426b43d8eadb7e46f1c0a5df8849d015620c4c
merveucer <merveucer.txt@gmail.com>
2021-06-05 03:01:00 +0300

    Bug fix

--- src/main/java/io/kodlama/hrms/entities/concretes/City.java
+++ src/main/java/io/kodlama/hrms/entities/concretes/City.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler"})
-@Table(name = "city")
+@Table(name = "cities")
 public class City {
 	
 	@Id
